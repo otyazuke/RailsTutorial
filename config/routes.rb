66cc8f0956261ec3ca6f626903b7dbee04a7b625
Takuya Tsuruda <takuya.tsuruda@dena.jp>
2017-05-16 09:45:05 +0900

    chapter 9

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
 
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
 
   resources :users
 
